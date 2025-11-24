42256e273d7b699a60bd3dd7f95d47a7fd81af10
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-11-24 23:21:45 +0000

    chnage les attributs de entity client

--- src/main/java/com/smartshop/smartshop/entity/Client.java
+++ src/main/java/com/smartshop/smartshop/entity/Client.java
@@ -4,22 +4,27 @@ import com.smartshop.smartshop.entity.enums.CustomerTier;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @Entity
 @Table(name = "clients")
-public class Client {
+public class Client extends User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String name;
 
     @Column(nullable = false, unique = true)
     private String email;
 
     @Enumerated(EnumType.STRING)
     private CustomerTier fidelityLevel;
+
+
+    private Integer totalOrders;
+
+    private BigDecimal totalSpent;
 
 }
